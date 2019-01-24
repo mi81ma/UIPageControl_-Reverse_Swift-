f853851ae749dbbc5f26fa4c5c2e22df37d10a0a
mi81ma <mi81ma@gmail.com>
2019-01-24 12:30:06 +0800

    Initial Commit

---
+++ UIPageControl_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageControl_test
+//
+//  Created by masato on 24/1/2019.
+//  Copyright © 2019 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
